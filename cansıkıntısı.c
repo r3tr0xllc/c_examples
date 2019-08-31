82318b1f54a49e3c63709de9b5985507c3027f50
r3tr0xllc <49368156+r3tr0xllc@users.noreply.github.com>
2019-08-31 23:43:00 +0300

    Update cansıkıntısı.c

--- cansıkıntısı.c
+++ cansıkıntısı.c
@@ -1,24 +1,75 @@
-//can sıkıntısından yazılmış beyin yakan boş bir c projesi
+//can sıkıntısından yazılmış beyin yakan boş c projeleri;
 #include "stdio.h"
+#include "stdlib.h"
 int main(int argc, char const *argv[]) {
-    int sayi1,sayi2,sayi3,sonuc;
-    printf("LÜTFEN İLK SAYİYİ GİRİNİZ :" );
-    scanf("%d",&sayi1 );
-    printf("LÜTFEN İKİNCİ SAYİYİ GİRİNİZ : ");
+  //////////////////////////////////////////////////////////////
+  /*
+    printf("BİRİBİRİNDEN FARKLİ İKİ ADET SAYİ GİRİNİZ : ");
+    int sayi1,sayi2;
+    scanf("%d",&sayi1);
     scanf("%d",&sayi2 );
-    printf("LÜTFEN ÜÇÜNCÜ SAYİYİ GİRİNİZ : ");
-    scanf("%d",&sayi3 );
-    printf("GİRDİĞİNİZ SAYİLAR SİRASİYLA : %d , %d , %d ",sayi1,sayi2,sayi3 );
     if (sayi1>sayi2) {
-      sonuc=sayi1;
-    }
-    else{
-      sonuc=sayi2;
-    }
-    if (sayi3>sonuc) {
-      printf("\nSONUÇ : %d\n",sayi3 );
+      printf("İLK GİRDİĞİNİZ SAYİ BÜYÜKTÜR.");
     } else {
-      printf("\nSONUÇ : %d\n",sonuc );
+      printf("İKİNCİ GİRDİĞİNİZ SAYİ BUYUKTUR." );
     }
+    */
+    /////////////////////////////////////////////////////////////////
+    /*
+    printf("DERS NOTU HESAPLAMA PROGRAMINA HOŞ GELDİNİZ LÜTFEN VİZE NOTUNUZU GİRİNİZ : ");
+    int vize_not;
+    scanf("%d",&vize_not );
+    printf("LÜTFEN FİLAN NOTUNUZU GİRİNİZ : ");
+    int final_not;
+    scanf("%d",&final_not);
+    int ortalama=(vize_not*0.40)+(final_not*0.60);
+    printf("NOT ORTALAMANIZ : %d\n",ortalama );
+    */
+    ///////////////////////////////////////////////////////////////////////////
+    /*
+    for (int i = 0; i < 100; i++) {
+      if (i%2==1) {
+        printf("%d\n",i );
+      }
+    }
+    */
+    ///////////////////////////////////////////////////////////////////
+    /*
+    float fiyat,kdvli;
+    printf("LÜTFEN FİYATI GİRİNİZ : ");
+    scanf("%f",&fiyat );
+    kdvli=((fiyat/100)*18 + fiyat);
+    printf("KDV Lİ FİYAT : %f \n",kdvli );
+    */
+    //////////////////////////////////////////////////////////////7
+    /*
+    printf("LÜTFEN RASTGELE 5 SAYI GİRİNİZ : ");
+    int ilk,ikinci,ucuncu,dorduncu,besinci,toplam;
+    scanf("%d",&ilk );
+    scanf("%d",&ikinci );
+    scanf("%d",&ucuncu );
+    scanf("%d",&dorduncu );
+    scanf("%d",&besinci );
+    toplam=ilk+ikinci+ucuncu+dorduncu+besinci;
+    float ort=toplam/5;
+    printf("GİRDİĞİNİZ SAYILARIM TOPLAMI : %d, ORTALAMASI : %f ",toplam,ort );
+    */
+    /*
+    int sayi,faktoriyel=1,i;
+     printf("Bir sayi giriniz:");
+     scanf("%d",&sayi);
+     i=sayi;
+     if(sayi > 0){
+     for(i;i>=1;i--){
+        faktoriyel *= i;
+     }
+     printf("Girilen sayinin faktoriyeli = %d\n",faktoriyel);}
+     else if (sayi==0){ //KULLANICININ GIRILEN SAYIYI 0 GIRME IHTIMALI
+         printf("Girilen sayinin faktoriyeli = %d",faktoriyel);
+     }
+     else{ //KULLANICININ 0 DAN KUCUK GIRME IHTIMALI
+         printf("0 dan kucuk girdiniz tekrar giriniz.\n");
+     }
+     */
   return 0;
 }
